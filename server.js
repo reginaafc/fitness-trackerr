0837743e9501f004addd88ec2e8018b132a44611
Regina Franco <79554515+reginaafc@users.noreply.github.com>
2021-08-07 18:26:52 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 3005;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+require("./routes/html")(app);
+require("./routes/api")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
